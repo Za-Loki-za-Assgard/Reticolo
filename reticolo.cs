ee694ec9c0abe4a4d8817ed25695d2d66f4e2154
VasilySavin <bm1183303@gmail.com>
2020-09-19 22:26:44 +0300

    first commit

--- reticolo.cs
+++ reticolo.cs
@@ -9,42 +9,10 @@ namespace ConsoleApp2
 {
     class Program
     {
-        static void numGuessing()
-        {
-            int num = Convert.ToInt32(Console.ReadLine());
-
-            if (num >= 1 && num <= 99)
-            {
-                int piv;
-                int max = 99;
-                int min = 1;
-
-                while (true)
-                {
-                    piv = (min + max) / 2;
-
-                    if (num == piv)
-                    {
-                        Console.Write("res: " + num + "\n");
-                        break;
-                    }
-
-                    if (num > piv)
-                    {
-                        min = piv;
-                    }
-
-                    else if (num < piv)
-                    {
-                        max = piv;
-                    }
-                }
-            }
-        }
 
         static void Main(string[] args)
         {
-            numGuessing();
+
         }
     }
 }
