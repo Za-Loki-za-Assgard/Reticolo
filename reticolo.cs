1541111dba52260c49150f1a2b1b55e06881b2b5
Za-Loki-za-Assgard <58191208+Za-Loki-za-Assgard@users.noreply.github.com>
2020-09-20 23:24:26 +0300

    Update reticolo.cs
    Added the factorial function

--- reticolo.cs
+++ reticolo.cs
@@ -9,10 +9,20 @@ namespace ConsoleApp2
 {
     class Program
     {
+        static void Factorial()
+        {
+            Console.WriteLine("Input calculated value");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int factorial = x;
+            for (int i = x - 1; i > 1; i--)
+                factorial *= i;
+            Console.WriteLine($"{x}!={ factorial}");
+            
+        }
 
         static void Main(string[] args)
         {
-
+            Factorial();
         }
     }
 }
